81d7218a253ba70128c28e3f2370aa784f660b4b
Hamna Kamran <74135890+hamnakamran10@users.noreply.github.com>
2021-05-18 08:54:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// var num1= 10;
+// var num2= 10;
+// num1--;//decremental operation
+// num2++;//incremental operation
+// console.log(num1,num2)
+//  var num3=10
+//  num3+=10;
+//  console.log(num3)
+
+//  var num4=20
+//  num4-=5;
+//  console.log(num4)
+
+//  //Test
+
+//  var A=10
+//  A+=5;
+//  console.log(A)
+
+//  var B=10
+//  B-=5;
+//  console.log(B)
+ //End
+
+ let c='anas';
+ let d='taha';
+ let space=' ';
+ console.log(c + space + d)
+
+ 
